2a6291bec747cd4b735b978cb801600f0452401a
DraganDjordjevic1 <32943386+DraganDjordjevic1@users.noreply.github.com>
2018-03-18 17:17:52 +1100

    Add files via upload

---
+++ Hobo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPOOProgram
+{
+      class Hobo : Person
+      {
+            public Hobo(string pFname, string pLname, string pGender) : base(pFname, pLname, pGender)
+            {
+
+            }
+
+            public Hobo(string pGender) : base(pGender)
+            {
+
+            }
+
+            public override string Work()
+            {
+                  return "Find A Place To Sleep";
+
+            }
+      }
+}
+
+
+
